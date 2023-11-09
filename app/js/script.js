da9c5723112c6a017fa5365dde7050dd6c5fdbe6
E-sims <livecultr@gmail.com>
2023-11-09 12:54:27 -0600

    Add dark/light mode logic

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello!')
+const radioButtons = document.querySelectorAll('input[name="theme"]')
+
+radioButtons.forEach((radio) => {
+  radio.addEventListener('change', () => {
+    if (radio.id === 'dark') {
+      document.body.classList.add('dark')
+      document.body.classList.remove('light')
+    } else {
+      document.body.classList.add('light')
+      document.body.classList.remove('dark')
+    }
+  })
+})
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("Hello!");
+"use strict";var radioButtons=document.querySelectorAll('input[name="theme"]');radioButtons.forEach((function(t){t.addEventListener("change",(function(){"dark"===t.id?(document.body.classList.add("dark"),document.body.classList.remove("light")):(document.body.classList.add("light"),document.body.classList.remove("dark"))}))}));
 //# sourceMappingURL=script.js.map
